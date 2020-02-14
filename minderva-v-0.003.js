ced97503ffb6888a0cf70357908d97ce7575b2b0
Science Illustration <scienceillustration@Dwaine-Bests-MacBook-Pro-2.local>
2020-02-14 08:46:21 -0800

    Added rudimentary tally logic to quiz. Changed working file to v0.003.

---
+++ minderva-v-0.003.js
@@ -0,0 +1,100 @@
+'use strict';
+
+// THIS VERSION:
+// - it should keep track of right and wrong answers for eventual Spaced System (just a tally for now?)
+// - Question and Answer editing logic should be seperate methods
+
+
+// SOMEDAY MAYBE:
+// - it should provide answer validation
+
+
+/* Cards Object */
+// NOTE: Find some way to protect the idCount number
+// research ways to make the property private
+
+let flashCards = {
+
+    // Unique Card Id
+    _idCount: 3,
+
+    // Card Collection (contains sample data)
+    // Structure: {uid: int, 'question: string, 'answer': string}
+    cards: [
+        {uid: 0, 'question': 'What is New Mexico\'s Capital?', 'answer': 'Santa Fe', 'correct': 0, 'incorrect': 0},
+        {uid: 1, 'question': 'What is Colorado\'s Capital?', 'answer': 'Denver', 'correct': 1, 'incorrect': 1},
+        {uid: 2, 'question': 'What is Wyoming\'s Capital?', 'answer': 'Chyenne', 'correct': 2, 'incorrect': 5},
+        {uid: 7, 'question': 'What is Britain\'s Capital?', 'answer': 'London', 'correct': 3, 'incorrect': 1},
+    ],
+
+    // Display card collection
+    displayCards: function() {
+        for (let card of this.cards) {
+            console.log(card);
+        }
+    },
+
+    // Get current Id
+    currentId: function() {
+        return this._idCount;
+    },
+
+    // Find index based on id of card
+    findIndex: function(id) {
+        return this.cards.findIndex( ({uid}) => uid === id);
+    },
+
+    // Apply current id to new card, create card, display cards
+    addCard: function (q, a) {
+        const id = this.currentId();
+        this.cards.push({uid: id, 'question': q, 'answer': a});
+        this._idCount += 1;
+        this.displayCards();
+    },
+
+    // Delete Card
+    deleteCard: function(id) {
+        let idx = this.findIndex(id);
+        console.log(idx);
+        if (idx !== -1) {
+            this.cards.splice(idx, 1);
+        } else {
+            return 'This card id doesn\'t exit';
+        }
+        this.displayCards();
+    },
+
+    // Edit Card
+    editCard: function(id) {
+        let index = this.findIndex(id);
+        if (index !== -1) {
+            let q = prompt('Enter an updated question');
+            let a = prompt('Enter an updated answer.');
+            this.cards[index]['question'] = q;
+            this.cards[index]['answer'] = a;
+        } else {
+            return 'Can\'t edit card. This card id doesn\'t exit';
+        }
+        this.displayCards();
+    },
+
+    quiz: function() {
+        for (let card of Object.keys(this.cards)) {
+
+            prompt(this.cards[card]['question']);
+            
+            let response = prompt(`The answer is ${this.cards[card]['answer']}. Were you correct?
+            Please type 'y' for yes or 'n' for no.`)
+            let correct = response.toLowerCase();
+            
+            if (correct === 'y') {
+                this.cards[card]['correct'] += 1;
+            } else if (correct === 'n') {
+                this.cards[card]['incorrect'] += 1;
+            } else {
+                prompt('Invalid input, please try again')
+                // Very rudimentary validation at this stage
+            }
+        }
+    },
+}
--- notes.md
+++ notes.md
@@ -1,1 +1,6 @@
-Random Notes for the Project
+Random Notes for the Project:
+Maybe
+
+
+v.03 small edits
+-  Changed edit to assign individual property values instead editing complete object
