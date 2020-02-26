0d53a4ef806b6449602abe84f702730ff641b650
dwainetrain <dwaine.best@gmail.com>
2020-02-26 14:55:03 -0800

    Starting mvc design.

---
+++ minderva-v-0.005.js
@@ -0,0 +1,18 @@
+'use strict';
+
+class Model{
+    constructor( ){ }
+}
+
+class View{
+    constructor(){}
+}
+
+class Controller{
+    constructor(model, view){
+        this.model = model
+        this.view = view
+    }
+}
+
+const app = new Controller(new Model(), new View())
